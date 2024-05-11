75112529123f684c1e48dcbac2aef1779756a835
Abdalrahman-Alhamod <abd.alrrahman.alhamod@gmail.com>
2024-05-11 06:15:54 +0300

    fix course reading view for large screens

--- lib/src/course_details/presentation/views/course_reading_view.dart
+++ lib/src/course_details/presentation/views/course_reading_view.dart
@@ -2,8 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter_super_html_viewer/flutter_super_html_viewer.dart';
 // import 'package:halim/core/test/test_course.dart';
-import 'package:halim/core/themes/app_colors.dart';
-import 'package:halim/core/widgets/custome_elevated_button.dart';
+import 'package:halim/core/widgets/bottom_sheet_button.dart';
 
 import '../../../../core/translations/local_keys.g.dart';
 import 'widgets/more_details_section/lessons/course_reading_app_bar.dart';
@@ -15,31 +14,18 @@ class CourseReadingView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CourseReadingAppBar(),
-      body: Container(
-        decoration: BoxDecoration(
-          color: AppColors.primaryColor.withAlpha(30),
-        ),
-        child: Column(
-          children: [
-            const Expanded(
-              // child: HtmlContentViewer(
-              //   htmlContent: TestCourse.readingHTML,
-              //   initialContentHeight: MediaQuery.of(context).size.height,
-              //   initialContentWidth: MediaQuery.of(context).size.width,
-              // ),
-              child:Text('HTML CONTENT'),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16.0,
-                vertical: 24,
-              ),
-              child: CustomElevatedButton(
-                onPressed: () {},
-                title: LocaleKeys.Buttons_next.tr(),
-              ),
-            ),
-          ],
+      bottomSheet: BottomSheetButton(
+        title: LocaleKeys.Buttons_next.tr(),
+        onPressed: () {},
+      ),
+      body: const Center(
+        child: Expanded(
+          // child: HtmlContentViewer(
+          //   htmlContent: TestCourse.readingHTML,
+          //   initialContentHeight: MediaQuery.of(context).size.height,
+          //   initialContentWidth: MediaQuery.of(context).size.width,
+          // ),
+          child: Text('HTML CONTENT'),
         ),
       ),
     );
