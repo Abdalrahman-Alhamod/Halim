9ac8c6e829dbc5ec4e1087027b4ebb7f7ffae1de
Abdalrahman-Alhamod <abd.alrrahman.alhamod@gmail.com>
2024-06-04 08:03:21 +0300

    add navigation to course details views

--- lib/src/home/presentation/views/widgets/card_course.dart
+++ lib/src/home/presentation/views/widgets/card_course.dart
@@ -1,7 +1,9 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:halim/core/translations/locale_keys.g.dart';
+import 'package:halim/core/utils/app_route.dart';
 import 'package:halim/core/utils/context_extensions.dart';
 import 'package:halim/src/home/presentation/views/functions/remove_bookmark_bottom_sheet.dart';
 
@@ -42,144 +44,147 @@ class _CardCourseState extends State<CardCourse> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 150,
-      decoration: BoxDecoration(
-        color: context.isDarkMode
-            ? AppColors.loginWithButtonDarkColor
-            : AppColors.lightFlatButtonColor,
-        borderRadius: BorderRadius.circular(32),
-      ),
-      child: Row(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15.0),
-            child: Container(
-              width: 110,
-              height: 110,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage(widget.imageUrl),
+    return GestureDetector(
+      onTap: () => GoRouter.of(context).push(AppRoute.kCourseDetailsView),
+      child: Container(
+        height: 150,
+        decoration: BoxDecoration(
+          color: context.isDarkMode
+              ? AppColors.loginWithButtonDarkColor
+              : AppColors.lightFlatButtonColor,
+          borderRadius: BorderRadius.circular(32),
+        ),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: Container(
+                width: 110,
+                height: 110,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(widget.imageUrl),
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        constraints: BoxConstraints(maxWidth: 160),
-                        padding: EdgeInsets.symmetric(
-                          vertical: 4,
-                          horizontal: 6,
-                        ),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(6),
-                          color: const Color.fromARGB(255, 113, 132, 204)
-                              .withOpacity(0.25),
-                        ),
-                        child: AutoSizeText(
-                          widget.category,
-                          maxLines: 1,
-                          minFontSize: 8,
-                          style: const TextStyle(
-                            color: Colors.blue,
-                            fontSize: 10,
-                            fontWeight: FontWeight.w700,
-                            overflow: TextOverflow.ellipsis,
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          constraints: BoxConstraints(maxWidth: 160),
+                          padding: EdgeInsets.symmetric(
+                            vertical: 4,
+                            horizontal: 6,
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(6),
+                            color: const Color.fromARGB(255, 113, 132, 204)
+                                .withOpacity(0.25),
+                          ),
+                          child: AutoSizeText(
+                            widget.category,
+                            maxLines: 1,
+                            minFontSize: 8,
+                            style: const TextStyle(
+                              color: Colors.blue,
+                              fontSize: 10,
+                              fontWeight: FontWeight.w700,
+                              overflow: TextOverflow.ellipsis,
+                            ),
                           ),
                         ),
-                      ),
-                      const Spacer(),
-                      IconButton(
-                        icon: Icon(
-                          _isBookmarked
-                              ? Icons.bookmark
-                              : Icons.bookmark_border_outlined,
-                          size: 28,
-                          color: _isBookmarked ? Colors.blue : Colors.blue,
-                        ),
-                        onPressed: widget.isEnabled
-                            ? () {
-                                if (_isBookmarked) {
-                                  showRemoveBookmarkBottomSheet(context);
+                        const Spacer(),
+                        IconButton(
+                          icon: Icon(
+                            _isBookmarked
+                                ? Icons.bookmark
+                                : Icons.bookmark_border_outlined,
+                            size: 28,
+                            color: _isBookmarked ? Colors.blue : Colors.blue,
+                          ),
+                          onPressed: widget.isEnabled
+                              ? () {
+                                  if (_isBookmarked) {
+                                    showRemoveBookmarkBottomSheet(context);
+                                  }
+                                  setState(() {
+                                    _isBookmarked = !_isBookmarked;
+                                  });
                                 }
-                                setState(() {
-                                  _isBookmarked = !_isBookmarked;
-                                });
-                              }
-                            : null,
-                      ),
-                    ],
-                  ),
-                  AutoSizeText(
-                    widget.name,
-                    style: TextStyle(
-                      color: MediaQuery.of(context).platformBrightness ==
-                              Brightness.dark
-                          ? Colors.white
-                          : Colors.black87,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
+                              : null,
+                        ),
+                      ],
                     ),
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 1,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        '\$${widget.price}  ',
-                        style: const TextStyle(
-                          color: Colors.blue,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500,
+                    AutoSizeText(
+                      widget.name,
+                      style: TextStyle(
+                        color: MediaQuery.of(context).platformBrightness ==
+                                Brightness.dark
+                            ? Colors.white
+                            : Colors.black87,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          '\$${widget.price}  ',
+                          style: const TextStyle(
+                            color: Colors.blue,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.star_half_rounded,
-                        color: Colors.amber[400],
-                        size: 25,
-                      ),
-                      Text(
-                        '  ${widget.evaluation}  | ',
-                        style: TextStyle(
-                          color: MediaQuery.of(context).platformBrightness ==
-                                  Brightness.dark
-                              ? Colors.white
-                              : Colors.black,
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal,
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.star_half_rounded,
+                          color: Colors.amber[400],
+                          size: 25,
                         ),
-                      ),
-                      Text(
-                        '${widget.followers} ${LocaleKeys.CourseDetails_students.tr()}',
-                        style: TextStyle(
-                          color: MediaQuery.of(context).platformBrightness ==
-                                  Brightness.dark
-                              ? Colors.white
-                              : Colors.black,
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal,
+                        Text(
+                          '  ${widget.evaluation}  | ',
+                          style: TextStyle(
+                            color: MediaQuery.of(context).platformBrightness ==
+                                    Brightness.dark
+                                ? Colors.white
+                                : Colors.black,
+                            fontSize: 14,
+                            fontWeight: FontWeight.normal,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                        Text(
+                          '${widget.followers} ${LocaleKeys.CourseDetails_students.tr()}',
+                          style: TextStyle(
+                            color: MediaQuery.of(context).platformBrightness ==
+                                    Brightness.dark
+                                ? Colors.white
+                                : Colors.black,
+                            fontSize: 14,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/src/my_courses/presentation/views/widget/follow_course.dart
+++ lib/src/my_courses/presentation/views/widget/follow_course.dart
@@ -1,7 +1,9 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:halim/core/translations/locale_keys.g.dart';
+import 'package:halim/core/utils/app_route.dart';
 import 'package:halim/core/utils/context_extensions.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 
@@ -43,104 +45,107 @@ class _FollowCourseState extends State<FollowCourse> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 150,
-      decoration: BoxDecoration(
-        color: context.isDarkMode
-            ? AppColors.loginWithButtonDarkColor
-            : AppColors.lightFlatButtonColor,
-        borderRadius: BorderRadius.circular(32),
-      ),
-      child: Row(
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(15, 15, 5, 15),
-            child: Container(
-              width: 110,
-              height: 110,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage(widget.imageUrl),
+    return GestureDetector(
+      onTap: () => GoRouter.of(context).push(AppRoute.kMyCourseDetailsView),
+      child: Container(
+        height: 150,
+        decoration: BoxDecoration(
+          color: context.isDarkMode
+              ? AppColors.loginWithButtonDarkColor
+              : AppColors.lightFlatButtonColor,
+          borderRadius: BorderRadius.circular(32),
+        ),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 15, 5, 15),
+              child: Container(
+                width: 110,
+                height: 110,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(widget.imageUrl),
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Spacer(
-                  flex: 2,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                  child: AutoSizeText(
-                    widget.name,
-                    style: TextStyle(
-                      color: MediaQuery.of(context).platformBrightness ==
-                              Brightness.dark
-                          ? Colors.white
-                          : Colors.black87,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                    ),
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 1,
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Spacer(
+                    flex: 2,
                   ),
-                ),
-                Spacer(
-                  flex: 1,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                  child: Text(
-                    '${widget.hours} ${LocaleKeys.CourseDetails_hrs.tr()} ${widget.min} ${LocaleKeys.CourseDetails_mins.tr()}',
-                    style: TextStyle(
-                      color: MediaQuery.of(context).platformBrightness ==
-                              Brightness.dark
-                          ? Colors.white54
-                          : Colors.black54,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w300,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: AutoSizeText(
+                      widget.name,
+                      style: TextStyle(
+                        color: MediaQuery.of(context).platformBrightness ==
+                                Brightness.dark
+                            ? Colors.white
+                            : Colors.black87,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
                     ),
                   ),
-                ),
-                Spacer(
-                  flex: 1,
-                ),
-                LinearPercentIndicator(
-                  alignment: MainAxisAlignment.start,
-                  width: 150.0,
-                  animation: true,
-                  lineHeight: 9.0,
-                  percent: widget.completedEpisodes / widget.allEpisodes,
-                  barRadius: const Radius.circular(10),
-                  backgroundColor: Colors.grey[300],
-                  progressColor: getProgressColro(
-                      widget.completedEpisodes / widget.allEpisodes),
-                  clipLinearGradient: true,
-                  trailing: Text(
-                    "${widget.completedEpisodes} / ${widget.allEpisodes}",
-                    style: TextStyle(
-                      color: MediaQuery.of(context).platformBrightness ==
-                              Brightness.dark
-                          ? Colors.white54
-                          : Colors.black54,
-                      fontSize: 12,
-                      fontWeight: FontWeight.w300,
+                  Spacer(
+                    flex: 1,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Text(
+                      '${widget.hours} ${LocaleKeys.CourseDetails_hrs.tr()} ${widget.min} ${LocaleKeys.CourseDetails_mins.tr()}',
+                      style: TextStyle(
+                        color: MediaQuery.of(context).platformBrightness ==
+                                Brightness.dark
+                            ? Colors.white54
+                            : Colors.black54,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w300,
+                      ),
                     ),
                   ),
-                ),
-                Spacer(
-                  flex: 2,
-                ),
-              ],
-            ),
-          )
-        ],
+                  Spacer(
+                    flex: 1,
+                  ),
+                  LinearPercentIndicator(
+                    alignment: MainAxisAlignment.start,
+                    width: 150.0,
+                    animation: true,
+                    lineHeight: 9.0,
+                    percent: widget.completedEpisodes / widget.allEpisodes,
+                    barRadius: const Radius.circular(10),
+                    backgroundColor: Colors.grey[300],
+                    progressColor: getProgressColro(
+                        widget.completedEpisodes / widget.allEpisodes),
+                    clipLinearGradient: true,
+                    trailing: Text(
+                      "${widget.completedEpisodes} / ${widget.allEpisodes}",
+                      style: TextStyle(
+                        color: MediaQuery.of(context).platformBrightness ==
+                                Brightness.dark
+                            ? Colors.white54
+                            : Colors.black54,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ),
+                  Spacer(
+                    flex: 2,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
