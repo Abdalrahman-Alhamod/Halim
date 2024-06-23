4f022523b5967e308b4bc5529151ef30493c6611
Alaa-Lababedi <master.alaa.aldeen@gmail.com>
2024-06-23 12:19:20 +0300

    fix LanguageView & main AppLocales.ar

--- lib/src/profile_settings/presentation/views/widget/language_listTile.dart
+++ lib/src/profile_settings/presentation/views/widget/language_listTile.dart
@@ -1,15 +1,9 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:halim/core/themes/app_colors.dart';
-import 'package:halim/core/translations/app_locales.dart';
-import 'package:halim/core/utils/app_route.dart';
-import 'package:halim/core/utils/context_extensions.dart';
 
-import '../../../../../core/functions/show_custom_dialog.dart';
+import 'package:halim/core/translations/app_locales.dart';
+
 import '../../../../../core/translations/locale_keys.g.dart';
-import '../../../../../core/widgets/custome_flat_button.dart';
-import '../../../../forgot_password/presentation/views/widget/accont_sucsses_dialog.dart';
 
 class LanguageListTile extends StatefulWidget {
   const LanguageListTile();
@@ -63,26 +57,6 @@ class _LanguageListTileState extends State<LanguageListTile> {
                 context.setLocale(Locale(AppLocales.ar));
               });
             },
-          ),
-        ),
-        Container(height: context.height * 0.58),
-        Center(
-          child: CustomFlatButton(
-            onPressed: () {
-              showCustomDialog(
-                  context: context,
-                  widget: AccontSucssesDialog(
-                    () {
-                      print('Loading complete!');
-                      GoRouter.of(context).push(AppRoute.kHome);
-                    },
-                  ));
-            },
-            title: LocaleKeys.FillYourProfile_continue.tr(),
-            width: MediaQuery.of(context).size.width * 0.90,
-            height: 60,
-            kTextcolor: AppColors.lightFlatButtonColor,
-            kBackgroundcolor: AppColors.primaryColor,
           ),
         ),
       ],
