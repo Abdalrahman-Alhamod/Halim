37615d09143125e095e7d6c3703d1081ba3d3c61
Abdalrahman-Alhamod <abd.alrrahman.alhamod@gmail.com>
2024-10-11 15:37:24 +0300

    fix carousel slider

--- lib/src/home/presentation/views/widgets/card_advertisement_local.dart
+++ lib/src/home/presentation/views/widgets/card_advertisement_local.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import '../../../../../core/assets/app_images.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
@@ -27,64 +28,64 @@ class _CardAdvertisementLocalState extends State<CardAdvertisementLocal> {
       padding: const EdgeInsets.all(15.0),
       child: Column(
         children: [
-          // _buildImageCarousel(screenSize, context),
+          _buildImageCarousel(screenSize, context),
           const SizedBox(height: 10),
-          // _buildPageIndicator(context),
+          _buildPageIndicator(context),
         ],
       ),
     );
   }
 
-  // Widget _buildImageCarousel(Size screenSize, BuildContext context) {
-  //   return Container(
-  //     width: screenSize.width * 0.90,
-  //     height: 150,
-  //     decoration: BoxDecoration(
-  //       borderRadius: BorderRadius.circular(20),
-  //     ),
-  //     child: ClipRRect(
-  //       borderRadius: BorderRadius.circular(20),
-  //       child: CarouselSlider.builder(
-  //         carouselController: _carouselController,
-  //         itemCount: _imageUrls.length,
-  //         itemBuilder: (context, index, realIndex) {
-  //           return Image.asset(
-  //             _imageUrls[index],
-  //             width: double.infinity,
-  //             height: double.infinity,
-  //             fit: BoxFit.cover,
-  //           );
-  //         },
-  //         options: CarouselOptions(
-  //           autoPlay: true,
-  //           autoPlayInterval: const Duration(seconds: 8),
-  //           enlargeCenterPage: true,
-  //           viewportFraction: 1.0,
-  //           onPageChanged: (index, reason) {
-  //             setState(() {
-  //               _currentIndex = index;
-  //             });
-  //           },
-  //         ),
-  //       ),
-  //     ),
-  //   );
-  // }
+  Widget _buildImageCarousel(Size screenSize, BuildContext context) {
+    return Container(
+      width: screenSize.width * 0.90,
+      height: 150,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: CarouselSlider.builder(
+          carouselController: _carouselController,
+          itemCount: _imageUrls.length,
+          itemBuilder: (context, index, realIndex) {
+            return Image.asset(
+              _imageUrls[index],
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.cover,
+            );
+          },
+          options: CarouselOptions(
+            autoPlay: true,
+            autoPlayInterval: const Duration(seconds: 8),
+            enlargeCenterPage: true,
+            viewportFraction: 1.0,
+            onPageChanged: (index, reason) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+          ),
+        ),
+      ),
+    );
+  }
 
-  // Widget _buildPageIndicator(BuildContext context) {
-  //   return AnimatedSmoothIndicator(
-  //     activeIndex: _currentIndex,
-  //     count: _imageUrls.length,
-  //     effect: ExpandingDotsEffect(
-  //       activeDotColor: context.isDarkMode ? Colors.blue : Colors.blue,
-  //       dotColor: Colors.grey,
-  //       dotHeight: 8,
-  //       dotWidth: 8,
-  //       spacing: 8,
-  //     ),
-  //     onDotClicked: (index) {
-  //       _carouselController.animateToPage(index);
-  //     },
-  //   );
-  // }
+  Widget _buildPageIndicator(BuildContext context) {
+    return AnimatedSmoothIndicator(
+      activeIndex: _currentIndex,
+      count: _imageUrls.length,
+      effect: ExpandingDotsEffect(
+        activeDotColor: context.isDarkMode ? Colors.blue : Colors.blue,
+        dotColor: Colors.grey,
+        dotHeight: 8,
+        dotWidth: 8,
+        spacing: 8,
+      ),
+      onDotClicked: (index) {
+        _carouselController.animateToPage(index);
+      },
+    );
+  }
 }
